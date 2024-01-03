bfe9e906c907e27def099565045a2e8deccf3784
pinklz <liz.manka@yahoo.com>
2024-01-02 19:04:50 -0800

    Combine forms.py and csv_reader.py into forms.py

--- forms.py
+++ forms.py
@@ -1,13 +1,8 @@
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import client, file, tools
+import csv
 
-import json
-
-'''
-Maybe store names instead and hope that people's names match 
-the sheet
-'''
 
 SCOPES = [
     "https://www.googleapis.com/auth/forms.body.readonly",
@@ -61,8 +56,6 @@ for word in s.split(','):
         name_bool = False
             
 
-print("-------------Setting questionId: ", questionId)
-print(name_id)
 
 usernames = []
 names = []
@@ -94,3 +87,53 @@ print(venmos)
 #print(form)
 #print("\n\nBRUH\n\n")
 #print(response)
+
+########## READ CSV VENMO TRANSACTION HISTORY ##########
+
+username = input("Input username: ")
+csv_file = input("Input csv file name: ")
+payments = []
+
+with open(csv_file, newline='') as csvfile:
+    file_reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in file_reader:
+        s = ' '.join(row)
+        if username in s:
+            end_name = s.find(username) + len(username)+1
+            if s[end_name] == '+':      #payment to me
+                
+                amount = s[end_name+3]
+                i = end_name
+                while s[i+4] != ',':        #how much
+                    i+=1
+                    amount += s[i+3]
+
+                start_name = s.find(username)
+                name = s[start_name-2]
+                j = start_name          #who paid
+                while s[j-3] != ',':
+                    j-=1
+                    name += s[j-2]
+                name = name[::-1]
+
+                j-=4
+                reason = ""             #reason
+                while s[j] != ",":
+                    reason+=s[j]
+                    j-=1
+                reason = reason[::-1]
+                
+
+                date_index = 1
+                while s[date_index] != ',':
+                    date_index+=1       #when it was paid
+                date_index+=1
+                date = ""
+                while s[date_index] != 'T':
+                    date += s[date_index]
+                    date_index+=1
+
+                payments.append(name + ":" + amount + ":" + date + ":" + reason)
+
+for i in payments:
+    print(i)
