6c6cb3a2a2c6ed26a264d4573af1da94152f2346
pinklz <liz.manka@yahoo.com>
2024-01-07 10:38:59 -0800

    Add new page to list unpaid venmo users

--- app.py
+++ app.py
@@ -7,15 +7,22 @@ def home():
     title = 'Treasurer Check'
     return render_template('home.html', title=title)
 
-@app.route('/test', methods=['POST'])
-def test():
+@app.route('/checklist', methods=['POST'])
+def checklist():
     title = 'Treasurer Check'
     form_id = request.form['form_id']
     username = request.form['username']
     csv_file = request.form['csv_file']
     venmos = forms.get_venmos(form_id)
     payments = forms.get_payments(username, csv_file)
-    return render_template('test.html', title=title, venmos=venmos, payments=payments)
+    return render_template('checklist.html', title=title, venmos=venmos, payments=payments)
+
+@app.route('/unpaid', methods=['POST','GET'])
+def unpaid():
+    title = 'Treasurer Check'
+    unpaidlist = request.form.getlist('unpaidchecks')
+    #TODO: add line for to get phone numbers from form to print on unpaid list
+    return render_template('unpaid.html', title=title, unpaidlist=unpaidlist)
 
 if __name__ == '__main__':
     app.run(debug=True)
